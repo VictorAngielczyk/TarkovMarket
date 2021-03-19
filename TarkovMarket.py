9964f7daa52bcec2996a868aeb5397a8e3c55870
Roberto Groza <robertogroza@gmail.com>
2021-03-19 16:20:02 -0400

    voice recognition (Google API Speech-to-Text) working; added text-to-speech using gTTS; added hotkey to enable recognition (Home)

--- TarkovMarket.py
+++ TarkovMarket.py
@@ -4,6 +4,7 @@ import os
 from rich.console import Console
 from rich.table import Table
 from SpeechRecognition import *
+import keyboard
 
 
 print("Made by Victor Angel#4300\n")
@@ -44,30 +45,32 @@ def getInfo(item, apiKey):
 
 while True:
     try:
-        price, symbol, item, traderPrice, traderName, changeD, changeW, slots = getInfo(speechToText(), apiKey)
+        if keyboard.is_pressed('home'):
+            price, symbol, item, traderPrice, traderName, changeD, changeW, slots = getInfo(str(speechToText()), apiKey)
+            textToSpeech(str(price))
 
-        table = Table(show_header=True, header_style="bold cyan")
-        table.add_column("Item")
-        table.add_column("Price")
-        table.add_column("Per Slot")
-        table.add_column("Trader")
-        table.add_column("Trader Price")
+            table = Table(show_header=True, header_style="bold cyan")
+            table.add_column("Item")
+            table.add_column("Price")
+            table.add_column("Per Slot")
+            table.add_column("Trader")
+            table.add_column("Trader Price")
 
-        if changeD >= 0:
-            table.add_column("Δ 24h", style="bold green")
-        else:
-            table.add_column("Δ 24h", style="bold red")
+            if changeD >= 0:
+                table.add_column("Δ 24h", style="bold green")
+            else:
+                table.add_column("Δ 24h", style="bold red")
 
-        if changeW >= 0:
-            table.add_column("Δ 7d", style="bold green")
-        else:
-            table.add_column("Δ 7d", style="bold red")
+            if changeW >= 0:
+                table.add_column("Δ 7d", style="bold green")
+            else:
+                table.add_column("Δ 7d", style="bold red")
 
-        table.add_row(item, "{}{:0,}".format(symbol, price), "{}{:0,}".format(symbol, int(price/slots)), traderName, "{}{:0,}".format(symbol, traderPrice), "{}%".format(changeD), "{}%".format(changeW))
-        
-        os.system("cls" if os.name == "nt" else "clear")
+            table.add_row(item, "{}{:0,}".format(symbol, price), "{}{:0,}".format(symbol, int(price/slots)), traderName, "{}{:0,}".format(symbol, traderPrice), "{}%".format(changeD), "{}%".format(changeW))
+            
+            os.system("cls" if os.name == "nt" else "clear")
 
-        out.print(table)
+            out.print(table)
 
     except Exception as e:
         print("Nothing found! / Error:",e)
